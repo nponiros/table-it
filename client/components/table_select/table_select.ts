a5b863ab8edb79843663be2005348d9af89cca52
Nikolas Poniros <github@poniros.de>
2015-07-19 11:22:52 +0200

    build the tables list using data received from the server

--- client/components/table_select/table_select.ts
+++ client/components/table_select/table_select.ts
@@ -1,19 +1,27 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
-import {Component, View} from 'angular2/angular2';
+import {Component, View, NgFor} from 'angular2/angular2';
+
+// TODO: examples say to use 'angular2/http' but module http is not declared in the typings
+import {Http} from 'angular2/angular2';
 
 import {Router} from 'angular2/router';
-
 
 @Component({
   selector: 'table-select'
 })
 @View({
-  templateUrl: './components/table_select/table_select.html'
+  templateUrl: './components/table_select/table_select.html',
+  directives: [NgFor]
 })
 export default class TableSelect {
-  constructor(private router: Router) {
+  private tables: Array<string>;
+  private router: Router;
+  private http: Http;
+  constructor(router: Router, http: Http) {
     this.router = router;
+    this.tables = [];
+    http.get('/api/v1/tables').toRx().map(res => res.json()).subscribe(tables => this.tables = tables)
   }
   navigate (whichTable) {
     this.router.navigate('/table/' + whichTable);
